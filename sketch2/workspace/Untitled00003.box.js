8af8a5d354745df6aa6abd88b33619f05f6a26ee
Alec Bargher <abargher@uchicago.edu>
2024-10-15 17:48:56 -0500

    working on sketch2 pythonhead pose estimation drawing

--- sketch2/workspace/Untitled00003.box.js
+++ sketch2/workspace/Untitled00003.box.js
@@ -1,1 +1,38 @@
-// hello there
+var ToAxi = Java.type("trace.ToAxi")
+
+var f = new FLine()
+var num = 400
+var f = new FLine()
+
+_.lines.clear()
+var f2 = new FLine()
+for(var i=0;i<num;i++)
+{
+	let alpha = i/num
+	//let theta = Math.PI*2*alpha
+	let theta = Math.PI*2*Math.random()
+	
+	var x = Math.sin(theta)*140
+	var y = Math.cos(theta)*130
+
+	var x2 = Math.sin(theta+1.0)*400
+	var y2 = Math.cos(theta+1.0)*400
+
+	f.moveTo(0,0)
+	f.polarCubicTo(1,100,0.23,100, x, y)
+	f.smoothTo(x2, y2)
+	
+	//f2.moveTo(0,0).lineTo(100,300)
+	//f2.moveTo(x,y).lineTo(500,500)
+	
+}
+
+//f2.pointed = true
+//f2.pointSize = 10
+_.lines.f = f
+//_.lines.f2 = f2
+_.redraw()
+
+var toaxi = new ToAxi("/Users/alec/CMSC/madd25604/curvything.csv")
+var paths = toaxi.sample(_.lines, 5.0)
+toaxi.export(paths, 1.0, a.fit(paths, vec(17, 11)), vec(0.5, 0.5), a.min(paths))
---
+++ sketch2/workspace/Untitled00004.box.js
@@ -0,0 +1,49 @@
+var PythonHead = Java.type("trace.video.PythonHead")
+// **change this according to your OS and according to where you put pythonhead**
+// PythonHead.start(_, "/Users/alec/CMSC/madd25604/sketch2/venv/bin/python3", "/Users/alec/CMSC/madd25604/sketch2/pythonhead", ["--do_lines", "--shrink", "0.2", "--video", "1", "--do_flow"])
+
+PythonHead.start(_, "/Users/alec/CMSC/madd25604/sketch2/venv/bin/python3", "/Users/alec/CMSC/madd25604/sketch2/pythonhead", 
+["--shrink", "0.5",
+ "--video", "1",
+ "--do_pose"])
+
+// do this every frame
+_r = () => {
+	var points = PythonHead.now.posepoints
+	var width = Math.floor(Number(PythonHead.now.width))
+	var height = Math.floor(Number(PythonHead.now.height))
+	
+	var draw_points = []
+	for (var coord of points[0]) {
+		var fx = Number(coord[0])
+		var fy = Number(coord[1])
+		var fz = Number(coord[2])
+		var loc = vec(fx * width, fy * height)
+		draw_points.push(loc)
+	}
+	
+	var newline = new FLine().data("ml*", draw_points)
+	newline.thicken = 10
+	newline.color = vec(0,0,0,1)
+	_.lines.f = newline
+	
+// 	var fx = PythonHead.now.mean_motion_x*10000
+// 	var fy = PythonHead.now.mean_motion_y*10000
+
+// 	var b = PythonHead.now.mean_red /255
+// 	var g = PythonHead.now.mean_green /255
+// 	var r = PythonHead.now.mean_blue /255
+
+// 	var px = PythonHead.now.mean_motion_px*960
+// 	var py = PythonHead.now.mean_motion_py*540
+
+// 	var f = new FLine()
+
+// 	f.moveTo(px, py)
+// 	f.lineTo(px+fx, py+fy)
+
+// 	f.thicken=10
+// 	f.color = vec(r,g,b,1)
+
+// 	_.lines.f = f
+}
