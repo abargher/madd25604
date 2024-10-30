77c11d30866c8a8f49c8dcbec3d9004f44f16b48
Alec Bargher <abargher@uchicago.edu>
2024-10-29 23:39:46 -0500

    alec: static trees

--- sketch3/PlantTree.cs
+++ sketch3/PlantTree.cs
@@ -41,18 +41,16 @@ public partial class PlantTree : Area2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (_growProgress < 1.0) {
-			_growProgress = Math.Min(_growProgress + growSpeed * (float)delta, 1.0f);
-			Scale = new Vector2(1, _growProgress);
-		}
-
 		var moveVelocity = new Vector2(x: 1, y: 0);
 
 		if (Input.IsActionPressed("wind_on")) {
 			moveVelocity = new Vector2(x: 2, y: 0);
 		}
 
-		if (Input.IsActionJustPressed("new_tree") && !createdBranches && RemainingBranches > 0) {
+		if (_growProgress < 1.0) {
+			_growProgress = Math.Min(_growProgress + growSpeed * (float)delta, 1.0f);
+			Scale = new Vector2(1, _growProgress);
+		} else if (!createdBranches && RemainingBranches > 0) {
 			for (int i = 0; i < rng.RandiRange(2, 4); i++) {
 				PlantTree branch = BranchScene.Instantiate<PlantTree>();
 				
@@ -70,7 +68,7 @@ public partial class PlantTree : Area2D
 			moveVelocity = moveVelocity.Normalized() * MoveSpeed;
 		}
 
-		Position += moveVelocity * (float)delta;
+		// Position += moveVelocity * (float)delta;
 	}
 
 	private void OnVisibleScreenNotifier2DScreenExited()
