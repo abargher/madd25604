4ab392c9cfa6c07adaa1702dd7347279727c899a
Alec Bargher <abargher@uchicago.edu>
2024-10-29 22:27:47 -0500

    alec: trees grow slowly now

--- sketch3/PlantTree.cs
+++ sketch3/PlantTree.cs
@@ -5,6 +5,8 @@ public partial class PlantTree : Area2D
 {
 	private const float MAX_GROW_VEL = 10f;
 
+	public float maxAngle = 10;
+
 	public PackedScene BranchScene = GD.Load<PackedScene>("res://plant_tree.tscn");
 
 	[Export]
@@ -20,20 +22,28 @@ public partial class PlantTree : Area2D
 
 	public float angle = 0;
 
+	private float growSpeed;
+	private float _growProgress = 0;
+
 	private RandomNumberGenerator rng = new RandomNumberGenerator();
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		angle = rng.RandfRange(-40, 40);
+		angle = rng.RandfRange(-maxAngle, maxAngle);
 		RotationDegrees = angle;
+		Scale = new Vector2(1, 0);
 
+		growSpeed = rng.RandfRange(0.7f, 0.9f);	
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		var growVelocity = rng.RandfRange(0, MAX_GROW_VEL);
+		if (_growProgress < 1.0) {
+			_growProgress = Math.Min(_growProgress + growSpeed * (float)delta, 1.0f);
+			Scale = new Vector2(1, _growProgress);
+		}
 
 		var moveVelocity = new Vector2(x: 1, y: 0);
 
@@ -42,12 +52,13 @@ public partial class PlantTree : Area2D
 		}
 
 		if (Input.IsActionJustPressed("new_tree") && !createdBranches && RemainingBranches > 0) {
-			for (int i = 0; i < rng.RandiRange(1, 3); i++) {
+			for (int i = 0; i < rng.RandiRange(2, 4); i++) {
 				PlantTree branch = BranchScene.Instantiate<PlantTree>();
 				
 				branch.Position = new Vector2(0, -150);
 				branch.MoveSpeed = 0;
 				branch.RemainingBranches = Math.Max(RemainingBranches - 1, 0);
+				branch.maxAngle = maxAngle + 15;
 
 				AddChild(branch);
 			}
