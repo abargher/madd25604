f041b8db9796de86f82f5c4983c19ca84e288afb
Alec Bargher <abargher@uchicago.edu>
2024-12-03 21:03:31 -0600

    tree manager script

--- final/scripts/TreeManager.cs
+++ final/scripts/TreeManager.cs
@@ -7,12 +7,16 @@ public partial class TreeManager : Node
 	[Export]
 	public int numTrees = 3;
 
+	public const Tuple<int, int> seedPodRange = new Tuple<int, int>(2, 5);
+
 	public int numSeedPods;
+	private RandomNumberGenerator gen = new RandomNumberGenerator();
+	
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-
+		numSeedPods = gen.RandiRange(seedPodRange.Item1, seedPodRange.Item2);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
